37cc9a271a9766885a3c551140e8cc8b49f05551
magyury <magyury@hotmail.com>
2018-03-15 17:26:33 +0100

    registration with email - first part completed

---
+++ webapp/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,111 @@
+<?php
+// REPLACED
+// use App\Models\User;
+// use Validator;
+// use App\Http\Controllers\Controller;
+// use Illuminate\Foundation\Auth\ThrottlesLogins;
+// use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+// WITH
+namespace App\Http\Controllers\Auth;
+use App\Factories\ActivationFactory;
+use App\Models\User;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    protected $activationFactory;
+
+    // REPLACED function __construct() WITH
+    public function __construct(ActivationFactory $activationFactory)
+    {
+        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+        $this->activationFactory = $activationFactory;
+    }
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, (new User)->rules());
+    }
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+
+    /* At the bottom of the controller, add these three functions after the create function.
+    / The register function we are adding will override the register method inherited 
+    / from the AuthenticatesAndRegistersUsers trait.
+    */
+    public function register(Request $request)
+    {
+        $validator = $this->validator($request->all());
+        if ($validator->fails()) {
+            $this->throwValidationException(
+                $request, $validator
+            );
+        }
+        $user = $this->create($request->all());
+        $this->activationFactory->sendActivationMail($user);
+        return redirect('/login')->with('activationStatus', true);
+    }
+
+    public function activateUser($token)
+    {
+        if ($user = $this->activationFactory->activateUser($token)) {
+            auth()->login($user);
+            return redirect($this->redirectPath());
+        }
+        abort(404);
+    }
+
+    public function authenticated(Request $request, $user)
+    {
+        if (!$user->activated) {
+            $this->activationFactory->sendActivationMail($user);
+            auth()->logout();
+            return back()->with('activationWarning', true);
+        }
+        return redirect()->intended($this->redirectPath());
+    }
+    
+}
--- webapp/resources/lang/en/auth.php
+++ webapp/resources/lang/en/auth.php
@@ -12,8 +12,12 @@ return [
     | these language lines according to your application's requirements.
     |
     */
-
+    
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    
+    // ADDED. For the activation status messages that we need to display to the user.  
+    'activationStatus' => 'We sent you an activation code. Please check your e-mail.',
+    'activationWarning' => 'You need to confirm your account. We have sent you an activation code, please check your e-mail.',
 
 ];
